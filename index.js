2b4406cab5adce7c07c65694ad64d76dcf96c45b
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-29 09:27:34 +0000

    first debate
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,96 @@
+/*
+ *  the goal to build an amazing short ai like think what is not an ai.
+ *
+ */
+
+
+
+const president = function(){
+     this.lockerRoom = function(level){
+         return lockerRoom('level');
+     }
+     let position = [
+         ['i'],
+         ['we'],
+         ['he'],
+         ['she'],
+         ['they']
+     ];
+     let contrast = [
+          ['no'],
+          ['yes']
+     ];
+     let controversal = [
+         '$$':'^^ is a ',
+         '€€':'^^ is',
+         '%%':'^^ have a'
+     ];
+     let ballastPerson = [
+         [
+             ' $$ terrible person.',
+             ' $$ stupid.',
+             ' $$ no nothing.',
+             ' $$ idiot. ',
+             ' $$ frait. ',
+             ' $$ lover educated.',
+             ' $$ userless'
+         ],
+         [
+            ' $$ doing an amazing job',
+            ' $$ amazing person.',
+            ' $$ freind of my.',
+            ' $$ lovely'
+         ]
+     ];
+     let ballastContry = [
+         [
+             ' $$ shithole plac.',
+             ' $$ 
+         ],
+         [
+            'We have a good relation ship with ^^ .',
+            '$$ a nice country',
+            '$$ '
+         ]
+
+     ];
+     let lockerRoom = function(level){
+         return lockerRoomAction[level][0]+' her '+lockerRoonmThings[level][0];
+     }
+     let lockerRoomThings = [
+         [
+             'hand'
+         ],
+         [
+             'eye',
+             'ear',
+             'face'
+         ],
+         [
+             'lips',
+             'legs'
+         ]
+         [
+            'boobs',
+            'tits',
+            'pussy'
+         ]
+     ];
+     let lockerRoomAction = [
+         [
+            'grab',
+            'touch'
+         ],
+         [
+            'eat',
+            'kiss'
+         ],
+         [
+           'fuck',
+           'lick',
+           'suck'
+        ]
+
+     ];
+}
+
